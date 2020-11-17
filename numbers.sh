f8a216f46b64461f83144d5472367f59b8ca45c3
Reese Quigley <rquigley@chapman.edu>
2020-11-17 10:03:30 -0800

    Adds numbers.sh

---
+++ numbers.sh
@@ -0,0 +1,16 @@
+#! /bin/bash
+# number.sh
+# Reese Quigley
+echo -n "Enter number : "
+read n
+
+rem=$(( $n % 2 ))
+
+if [ $rem -eq 0 ]
+then
+   echo "$n is even number"
+else
+   echo "$n is odd number"
+fi
+echo "Thank you"
+
